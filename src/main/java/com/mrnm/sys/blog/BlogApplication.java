4a4cb2ddd8f6ab13222503a2273aee43cd36ec64
MauMau15 <mauriciorenero@gmail.com>
2025-09-09 23:16:42 -0600

    Initial commit!

---
+++ src/main/java/com/mrnm/sys/blog/BlogApplication.java
@@ -0,0 +1,22 @@
+package com.mrnm.sys.blog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class BlogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogApplication.class, args);
+	}
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello World!";
+    }
+
+}
